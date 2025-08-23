145c4faebeec7bc23657d6759a683b6332e1a69f
Daniel Arpide <itstoxsec@gmail.com>
2025-08-22 22:55:15 -0400

    arch updates

--- Scripts/arch.sh
+++ Scripts/arch.sh
@@ -1,5 +1,31 @@
-sudo pacman -S git zsh waybar polybar brightnessctl unzip nfts-3g neovim fastfetch swaybg nitrogen wlsunset slurp grim distrobox docker
+#!/bin/bash
+
+mapfile -t packages < packages.txt
+
+echo "Installing applications..."
+    for pkg in "${packages[@]}"; do
+        echo "Installing $pkg..."
+        sudo pacman -S --noconfirm "$pkg"
+        if [ $? -ne 0 ]; then
+            echo "Error installing $pkg. Skipping."
+        fi
+    done
+
+echo "Application installation complete."
 
 sudo usermod -aG docker ${USER}
 
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+yay -S \
+    brave-bin \
+    hyprshade \
+    lunar-client \
+    spotify \
+    vmware-keymaps \
+    vmware-host-module-dkms-git \
+    vmware-host-modules-dkms-fix-git \
+    vmware-workstation \
+    wireshark-qt
--- Scripts/links.sh
+++ Scripts/links.sh
@@ -1,6 +1,6 @@
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
-ln -s ~/dotfiles/hypr ~/.config/hypr
+ln -s ~/dotfiles/hypr ~/.config/
 ln -s ~/dotfiles/i3 ~/.config/i3
 ln -s ~/dotfiles/polybar ~/.config/polybar
 ln -s ~/dotfiles/waybar ~/.config/waybar
@@ -10,5 +10,5 @@ ln -s ~/dotfiles/neofetch ~/.config/neofetch
 ln -s ~/dotfiles/terminator ~/.config/terminator
 ln -s ~/dotfiles/wofi ~/.config/wofi
 sudo ln -s ~/dotfiles/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
-sudo ln -s ~/dotfiles/nix-configs/configuration.nix /etc/nixos/configuration.nix
-sudo ln -s ~/dotfiles/nix-configs/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
+# sudo ln -s ~/dotfiles/nix-configs/configuration.nix /etc/nixos/configuration.nix
+# sudo ln -s ~/dotfiles/nix-configs/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
