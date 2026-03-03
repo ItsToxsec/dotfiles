d02ef0315d8508797fba982682898301ae8e057a
Daniel Arpide <itstoxsec@gmail.com>
2026-03-02 20:30:47 -0500

    updates

--- Scripts/mountNas.sh
+++ Scripts/mountNas.sh
@@ -1,1 +1,1 @@
-sudo mount -t cifs //10.0.150.1:/NAS1/txs-nas1 /home/itstoxsec/NAS -o username=$s1,pass=$s2,vers=2.0
+sudo mount -t cifs //10.0.150.1/txs-nas1 /home/itstoxsec/NAS -o username=$1,pass=$2,vers=2.0,uid=1000,gid=100
