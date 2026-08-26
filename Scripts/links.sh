053aaf9f27dcac4dd52512bea1464d4897462db4
Daniel Arpide <itstoxsec@gmail.com>
2026-08-25 21:43:16 -0400

    Removed unused dotfile folders

--- Scripts/links.sh
+++ Scripts/links.sh
@@ -1,15 +1,11 @@
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/Shells/.zshrc ~/.zshrc
+ln -s ~/dotfiles/Shells/.bashrc ~/.bashrc
 ln -s ~/dotfiles/hypr ~/.config/hypr
-ln -s ~/dotfiles/i3 ~/.config/i3
-ln -s ~/dotfiles/polybar ~/.config/polybar
 ln -s ~/dotfiles/waybar ~/.config/waybar
 ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/picom ~/.config/picom
-ln -s ~/dotfiles/neofetch ~/.config/neofetch
 ln -s ~/dotfiles/terminator ~/.config/terminator
 ln -s ~/dotfiles/wofi ~/.config/wofi
-ln -s ~/dotfiles/mouseless ~/.config/mouseless
+ln -s ~/dotfiles/quickshell ~/.config/quickshell
 sudo ln -s ~/dotfiles/40-libinput.conf /etc/X11/xorg.conf.d/40-libinput.conf
 sudo ln -s ~/dotfiles/nix-configs/configuration.nix /etc/nixos/configuration.nix
 sudo ln -s ~/dotfiles/nix-configs/hardware-configuration.nix /etc/nixos/hardware-configuration.nix
