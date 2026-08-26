90bd6303c727f7decc9ef1cf941bf7d05a022b70
Daniel <itstoxsec@gmail.com>
2026-08-26 10:44:12 -0400

    Update arch.sh

--- Scripts/arch.sh
+++ Scripts/arch.sh
@@ -1,5 +1,3 @@
 sudo pacman -S git zsh waybar polybar brightnessctl unzip nfts-3g neovim fastfetch swaybg nitrogen wlsunset slurp grim distrobox docker
 
 sudo usermod -aG docker ${USER}
-
-pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
