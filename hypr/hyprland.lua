c996843a807e84a8ee73e77cb7a7bc0d5a59f500
Daniel Arpide <itstoxsec@gmail.com>
2026-09-01 19:11:29 -0400

    hyprland auto-start update

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -524,6 +524,7 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("brave")
     hl.exec_cmd("hyprpaper")
     hl.exec_cmd("qs")
+    hl.exec_cmd("mouseless")
 end)
 
 -- Exec (run every reload)
